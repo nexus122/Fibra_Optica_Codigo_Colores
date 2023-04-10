dc30c838ccf93fbff3af29e9dfc3f7e0ff26b0da
nexus122 <juan-pablo122@hotmail.es>
2023-04-10 22:55:28 +0200

    Generar las marcas de los cables

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 class app {
-  constructor() {}
+  constructor() {
+    this.countryFilter();
+  }
   // Html Elements
   companySelect = document.querySelector("#compañia");
   cableSelect = document.querySelector("#cableSelect");
@@ -29,6 +31,7 @@ class app {
     // Escuchar el valor de la compañia
     this.companySelect.addEventListener("change", (event) => {
       this.companySelected = event.target.value; // Guardamos el valor seleccionado en la variable
+      this.countryFilter();
     });
 
     // Escuchamos el tipo de cable
@@ -49,6 +52,16 @@ class app {
     this.deleteBtn.addEventListener("click", () => {
       this.resultPanel.innerHTML = "";
     });
+  }
+
+  countryFilter() {
+    if (this.companySelected == "Orange") {
+      $(".fr").show();
+      $(".es").hide();
+    } else {
+      $(".fr").hide();
+      $(".es").show();
+    }
   }
 
   searchData() {
@@ -77,12 +90,60 @@ class app {
   drawResults(results) {
     this.resultPanel.innerHTML += `
       <article>
-      <h1>${this.companySelected} - ${this.cableSelected}</h1>
-        <p> El numero <b>${results[0]}</b></p>
-        <p><b>Tubo</b> <i style="color:${results[1]}"class="fa-sharp fa-solid fa-square"></i></p>
-        <p><b>Fibra</b> <i style="color:${results[2]}"class="fa-sharp fa-solid fa-square"></i></p>
+      <h1>${this.companySelected} - ${this.cableSelected} - ${results[0]}</h1>
+        <h3 class="tubo"><b>Tubo</b> <i style="color:${results[1]}"class="fa-sharp fa-solid fa-square"></i></h3>
+        <h3 class="fibra"><b>Fibra</b> <i style="color:${results[2]}"class="fa-sharp fa-solid fa-square"></i></h3>
       </article>
     `;
+    generarMarcas(this.cableSelected);
+  }
+}
+
+function generarMarcas(tipoCable) {
+  console.log("Entra");
+
+  let numero = $("#numero").val();
+  let n = document.querySelectorAll("article .tubo").length;
+  let lastArticle = document.querySelectorAll("article .tubo")[n - 1];
+
+  if (tipoCable == "256 F.O PKP") {
+    if (parseInt(numero) >= 13) {
+      let span = document.createElement("span");
+      span.className = "badge";
+      lastArticle.append(span);
+      span.append(`Primero __/`);
+    }
+  } else if (tipoCable == "512 F.O PKP") {
+    if (parseInt(numero) >= 9 && numero < 17) {
+      let span = document.createElement("span");
+      span.className = "badge";
+      lastArticle.append(span);
+      span.append(`Primero /`);
+    } else if (parseInt(numero) >= 17 && numero < 25) {
+      let span = document.createElement("span");
+      span.className = "badge";
+      lastArticle.append(span);
+      span.append(`Segundo / /`);
+    } else if (parseInt(numero) >= 25) {
+      let span = document.createElement("span");
+      span.className = "badge";
+      lastArticle.append(span);
+      span.append(`Tercero / / /`);
+    }
+  } else if (tipoCable == "144 F.O Francia") {
+    if (parseInt(numero) >= 97) {
+      let span = document.createElement("span");
+      span.className = "badge";
+      lastArticle.append(span);
+      span.append(`Primero /`);
+    }
+  } else if (tipoCable == "288 F.O Francia") {
+    if (parseInt(numero) >= 193) {
+      let span = document.createElement("span");
+      span.className = "badge";
+      lastArticle.append(span);
+      span.append(`Primero /`);
+    }
   }
 }
 
--- redme.md
+++ redme.md
@@ -1,3 +1,8 @@
 # Codigo de colores
 Esto es un programa que pretende ayudar a los instaladores de fibra optica.
 Mediante una sencilla interfaz puedes seleccionar compañia y tipo de cable, introducir un numero.
+
+## TODO [ ]
+- [] Cambiar el color de los cables en función de si tienen pines en Telefonica y Yastel para el numero 512
+    - [] Si tienen pines el color del tubo debe ser blanco en lugar de azul
+    - [] Comprobar si es si tienen dos pines minimo
