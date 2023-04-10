2b81c33f595b2c4c5ed1385270cb67dd20a068d2
nexus122 <juan-pablo122@hotmail.es>
2023-04-10 20:00:48 +0200

    [refactor] fix: Alterar la fuente de ifnormación a un json unificado

--- index.js
+++ index.js
@@ -1,209 +1,90 @@
-/* Tipos de cable */
-var cableInfo = [
-    [32, 4, 8, 0],
-    [48, 6, 8, 1],
-    [64, 8, 8, 2],
-    [96, 12, 8, 3],
-    [128, 16, 8, 4],
-    [256, 16, 16, 5],
-    [512, 16, 32, 6],
-    [144, 18, 8, 7],
-    [288, 18, 16, 8],
-]
+class app {
+  constructor() {}
+  // Html Elements
+  companySelect = document.querySelector("#compañia");
+  cableSelect = document.querySelector("#cableSelect");
+  numeroInput = document.querySelector("#numero");
+  searchBtn = document.querySelector("#buscar");
+  deleteBtn = document.querySelector("#borrar");
+  resultPanel = document.querySelector("#resultados");
+  // Vars and data
+  data = [];
+  companySelected;
+  cableSelected;
+  numeroSelected;
 
-var cables = [
-    "32 F.O PKP",
-    "48 F.O PKP",
-    "64 F.O PKP",
-    "96 F.O PKP",
-    "128 F.O PKP",
-    "256 F.O PKP",
-    "512 F.O PKP",
-    "144 F.O Francia",
-    "288 F.O Francia",
-]
+  async init() {
+    this.ObtainData();
+    this.HtmlInteractions();
+  }
 
-/* En ingles para el color */
-var tubeColorInfo = [
-    ["white", "red", "blue", "green"], // 32
-    ["white", "white", "red", "red", "blue", "blue"], // 48
-    ["white", "white", "red", "red", "blue", "blue", "green", "green"], // 64
-    ["white", "white", "white", "red", "red", "red", "blue", "blue", "blue", "green", "green", "green"], // 96
-    ["white", "red", "blue", "green", "white", "white", "white", "red", "red", "red", "blue", "blue", "blue", "green", "green", "green"], // 128
-    ["white", "red", "blue", "green", "white", "white", "white", "red", "red", "red", "blue", "blue", "blue", "green", "green", "green"], // 258
-    ["white", "red", "blue", "green", "white", "white", "white", "red", "red", "red", "blue", "blue", "blue", "green", "green", "green"],  // 512   
-    ["red", "blue", "green", "yellow", "purple", "white", "orange", "grey", "brown", "black", "cyan", "pink", "red", "blue", "green", "yellow", "purple", "white"],  // 144 F.O Francia    
-    ["red", "blue", "green", "yellow", "purple", "white", "orange", "grey", "brown", "black", "cyan", "pink", "red", "blue", "green", "yellow", "purple", "white"]  // 288 F.O Francia    
-]
+  async ObtainData() {
+    const infoResponse = await fetch("./data.json");
+    const cableInfo = await infoResponse.json();
+    this.data = cableInfo;
+    console.log(this.data);
+  }
 
-var fibraColorInfo = [
-    ["green", "red", "blue", "yellow", "grey", "purple", "brown", "orange"], // 32
-    ["green", "red", "blue", "yellow", "grey", "purple", "brown", "orange"], // 48
-    ["green", "red", "blue", "yellow", "grey", "purple", "brown", "orange"], // 64
-    ["green", "red", "blue", "yellow", "grey", "purple", "brown", "orange"], // 96
-    ["green", "red", "blue", "yellow", "grey", "purple", "brown", "orange"], // 128
-    ["green", "red", "blue", "yellow", "grey", "purple", "brown", "orange", "white", "black", "pink", "cyan", "white", "yellow", "orange", "pink"], // 256
-    ["green", "red", "blue", "yellow", "grey", "purple", "brown", "orange", "green", "red", "blue", "yellow", "grey", "purple", "white", "orange", "green", "red", "blue", "yellow", "grey", "purple", "white", "orange", "green", "red", "blue", "yellow", "grey", "purple", "white", "orange"],  // 512
-    ["red", "blue", "green", "yellow", "purple", "white", "orange", "grey"],  // 144 Francia
-    ["red", "blue", "green", "yellow", "purple", "white", "orange", "grey", "brown", "black", "cyan", "pink", "red", "blue", "green", "yellow"]  // 288 Francia
-]
-
-/* En español para las frases */
-var espTubeColorInfo = [
-    ["Blanco", "Rojo", "Azul", "Verde"], // 32
-    ["Blanco", "Blanco", "Rojo", "Rojo", "Azul", "Azul"], // 48
-    ["Blanco", "Blanco", "Rojo", "Rojo", "Azul", "Azul", "Verde", "Verde"], // 64
-    ["Blanco", "Blanco", "Blanco", "Rojo", "Rojo", "Rojo", "Azul", "Azul", "Azul", "Verde", "Verde", "Verde"], // 96
-    ["Blanco", "Rojo", "Azul", "Verde", "Blanco", "Blanco", "Blanco", "Rojo", "Rojo", "Rojo", "Azul", "Azul", "Azul", "Verde", "Verde", "Verde"], // 128
-    ["Blanco", "Rojo", "Azul", "Verde", "Blanco", "Blanco", "Blanco", "Rojo", "Rojo", "Rojo", "Azul", "Azul", "Azul", "Verde", "Verde", "Verde"], // 258
-    ["Blanco", "Rojo", "Azul", "Verde", "Blanco", "Blanco", "Blanco", "Rojo", "Rojo", "Rojo", "Azul", "Azul", "Azul", "Verde", "Verde", "Verde"],  // 512   
-    ["Rojo", "Azul", "Verde", "Amarillo", "Violeta", "Blanco", "Naranja", "Gris", "Marron", "Negro", "Turquesa", "Rosa", "Rojo", "Azul", "Verde", "Amarillo", "Violeta", "Blanco"], // 144 Francia
-    ["Rojo", "Azul", "Verde", "Amarillo", "Violeta", "Blanco", "Naranja", "Gris", "Marron", "Negro", "Turquesa", "Rosa", "Rojo", "Azul", "Verde", "Amarillo", "Violeta", "Blanco"] // 288 Francia       
-
-]
-
-var espFibraColorInfo = [
-    ["Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Marron", "Naranja"], // 32
-    ["Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Marron", "Naranja"], // 48
-    ["Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Marron", "Naranja"], // 64
-    ["Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Marron", "Naranja"], // 96
-    ["Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Marron", "Naranja"], // 128
-    ["Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Marron", "Naranja", "Blanco", "Negro", "Rosa", "Cyan", "Blanco", "Amarillo", "Naranja", "Rosa"], // 256
-    ["Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Marron", "Naranja", "Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Blanco", "Naranja", "Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Blanco", "Naranja", "Verde", "Rojo", "Azul", "Amarillo", "Gris", "Violeta", "Blanco", "Naranja"],  // 512
-    ["Rojo", "Azul", "Verde", "Amarillo", "Violeta", "Blanco", "Naranja", "Gris"], // 144 Francia
-    ["Rojo", "Azul", "Verde", "Amarillo", "Violeta", "Blanco", "Naranja", "Gris", "Marron", "Negro", "Turquesa", "Rosa", "Roja", "Azul", "Verde", "Amarillo"] // 288 Francia
-]
-
-
-/* FUNCIONES */
-
-// Funcion para crear las tablas
-function CreateTable(cableInfo) {
-    var cable = cableInfo[0]; fibras = cableInfo[1]; tubos = cableInfo[2];
-    console.log(`Cable: ${cable}, fibras: ${fibras} Tubo: ${tubos}`);
-    var arrTemp = []; arrTubo = [];
-
-    /* Bucle para llenar la array con todos los numeros */
-    for (var i = 0; i < (fibras * tubos); i++) {
-        var aux = i + 1;
-        arrTemp.push(aux);
-    }
-
-    /* Bucle para poder partir el array y construir las columnas */
-    var division = tubos;
-
-    for (let i = 0; i < arrTemp.length; i += division) {
-        let pedazo = arrTemp.slice(i, i + division);
-        arrTubo.push(pedazo);
-    }
-
-    return arrTubo
-
-}
-
-function search(table, numero, cableInfo) {
-    var temp = 0;
-    var color = cableInfo[3];
-    console.log("Color: ", color);
-    var result = [];
-    table.forEach((element, index) => {
-        var position = element.indexOf(parseInt(numero));
-        if (position != -1) {
-            console.log(`Tubo: ${index + 1} Color: ${tubeColorInfo[color][index]} Fibra: ${position + 1} Color: ${fibraColorInfo[color][position]}`);
-            result = [index + 1, tubeColorInfo[color][index], espTubeColorInfo[color][index], position + 1, fibraColorInfo[color][position], espFibraColorInfo[color][position]]
-        }
+  HtmlInteractions() {
+    // Escuchar el valor de la compañia
+    this.companySelect.addEventListener("change", (event) => {
+      this.companySelected = event.target.value; // Guardamos el valor seleccionado en la variable
     });
-    return result
+
+    // Escuchamos el tipo de cable
+    this.cableSelect.addEventListener("change", (event) => {
+      this.cableSelected = event.target.value;
+    });
+
+    // Escuchamos el numero
+    this.numeroInput.addEventListener("change", (event) => {
+      this.numeroSelected = event.target.value;
+    });
+
+    // Escuchamos a los botones:
+    this.searchBtn.addEventListener("click", () => {
+      this.drawResults(this.searchData());
+    });
+
+    this.deleteBtn.addEventListener("click", () => {
+      this.resultPanel.innerHTML = "";
+    });
+  }
+
+  searchData() {
+    if (!this.cableSelected || !this.numeroSelected) return;
+
+    const data = this.data.find(
+      (element) => element.name == this.cableSelected
+    );
+
+    const firstLength = data.cableInfo.fibers / data.cableInfo.tubes;
+    const arrTable = Array.from(
+      { length: firstLength },
+      (_, i) => data.cableInfo.fiberColors[i % 8]
+    ).flatMap((colors) => Array(data.cableInfo.tubes).fill(colors));
+
+    const tubeNumber = Math.ceil(this.numeroSelected / firstLength);
+    const fiberColor = arrTable[this.numeroSelected - 1];
+
+    return [
+      this.numeroSelected,
+      data.cableInfo.tubeColors[tubeNumber - 1],
+      fiberColor,
+    ];
+  }
+
+  drawResults(results) {
+    this.resultPanel.innerHTML += `
+      <article>
+      <h1>${this.companySelected} - ${this.cableSelected}</h1>
+        <p> El numero <b>${results[0]}</b></p>
+        <p><b>Tubo</b> <i style="color:${results[1]}"class="fa-sharp fa-solid fa-square"></i></p>
+        <p><b>Fibra</b> <i style="color:${results[2]}"class="fa-sharp fa-solid fa-square"></i></p>
+      </article>
+    `;
+  }
 }
 
-function dibujar(result, numero, cable) {
-    var d = new Date();
-    var n = d.getTime();
-    $("#resultados").append(`<hr>`);
-    $("#resultados").append(`<h3>Cable: <b>${cables[cable]}</b> Numero: <b>${numero}</b></h3>`);
-    $("#resultados").append(`<h3>Tubo <b>${result[0]}</b> Color <i class="fas fa-square-full" style="color:${result[1]}"></i> ${result[2]}</h3>`);
-    $("#resultados").append(`<h3>Fibra <b>${result[3]}</b> Color <i class="fas fa-square-full" style="color:${result[4]}"></i> ${result[5]} <span class="marca_${n}"></span></h3>`)
-    generarMarcas(cables[cable], result, n);
-}
-
-function generarMarcas(tipoCable, result, n) {
-    if (tipoCable == "256 F.O PKP") {
-        if (parseInt(result[3]) >= 13) {
-            $(".marca_" + n).append(`primero __/`);
-        }
-    }
-
-    if (tipoCable == "512 F.O PKP") {
-        if (parseInt(result[3]) >= 9 && result[3] < 17) {
-            $(".marca_" + n).append(`primero /`);
-        } else if (parseInt(result[3]) >= 17 && result[3] < 25) {
-            $(".marca_" + n).append(`segundo / /`);
-        } else if (parseInt(result[3]) >= 25) {
-            $(".marca_" + n).append(`tercero / / /`);
-        }
-    }
-
-    if (tipoCable == "144 F.O Francia") {
-        if (parseInt(result[3]) >= 97) {
-            $(".marca_" + n).append(`primero /`);
-        }
-    }
-
-    if (tipoCable == "288 F.O Francia") {
-        if (parseInt(result[3]) >= 193) {
-            $(".marca_" + n).append(`primero /`);
-        }
-    }
-}
-
-function error(mensaje){
-    $("#resultados").prepend(`<div class="alert alert-danger" role="alert">${mensaje}</div>`)
-}
-
-/* EVENTOS */
-
-/* Evento click para buscar */
-$("#buscar").click(async function () {
-    var cable = $("#cableSelect").val();
-    var numero = $("#numero").val();
-    var company = $("#compañia").val();
-    
-    console.log(company)
-
-    if(company == "" || company == null){
-        error("Debes elegir una compañia")
-    }else if(cable == "" || cable == null){
-        error("Debes elegir un tipo de cable")       
-    }else if(numero == "" || numero == null){
-        error("Debes escribir un numero")       
-    }
-    else if(numero > cableInfo[cable][0]){
-       error("El numero es mas grande que el maximo del cable")       
-    }else{
-        $(".alert-danger").remove();
-         /* Llamamos a las funciones */
-         var table = CreateTable(cableInfo[cable]);    
-
-         var result = search(table, numero, cableInfo[cable]);
-         console.log(result);
-         dibujar(result, numero, cable);
-    }
-})
-
-$("#borrar").click(function () {
-    $("#resultados").html("");
-})
-
-
-$(".fr").hide(); // Escondemos las opciones de francia
-
-/* Ocultamos o mostramos en funcion de la compañia */
-$("#compañia").change(function () {
-    if ($("#compañia").val() == 0) {
-        $(".es").hide();
-        $(".fr").show();
-    } else if ($("#compañia").val() == 1) {
-        $(".fr").hide();
-        $(".es").show();
-    }
-})
+const myApp = new app();
+myApp.init();
